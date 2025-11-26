a62e2d37a2b5287f67474e342ac7690ea0bff51e
Rafly Yusuf <107983288+rafly1012@users.noreply.github.com>
2025-11-26 23:33:31 +0800

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://server-presensi.vercel.app/api",
   withCredentials: true,
 });
 
