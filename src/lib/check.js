0dd4e88e29d36c381f08ee706834394e6c2fd8ed
Rafly Yusuf <107983288+rafly1012@users.noreply.github.com>
2026-01-22 18:24:13 +0800

    Update check.js

--- src/lib/check.js
+++ src/lib/check.js
@@ -1,6 +1,6 @@
 export const TIME_RANGES = {
   jammasuk: { start: "10:12", end: "10:15" },
-  jamkeluar: { start: "10:17", end: "10:20" },
+  jamkeluar: { start: "10:17", end: "18:30" },
 };
 
 export const DAYS_OF_WEEK = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
